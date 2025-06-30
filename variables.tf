fe2735b4fe692bb889c4d4e753ad1b77b9fa6063
OnTrak-Tech <adjeigideon911@gmail.com>
2025-06-30 16:24:58 +0000

    Initial commit

--- variables.tf
+++ variables.tf
@@ -1,67 +1,34 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+variable "instance_type" {
+  description = "EC2 instance type"
   type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.20.0/24"]
-}
-
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["eu-west-1a", "eu-west-1b"]
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "LampStack.pem"
+  description = "AWS Key Pair name"
   type        = string
-}
-
-variable "web_instance_type" {
-  description = "Instance type for web servers"
-  type        = string
-  default     = "t3.micro"
 }
 
 variable "db_name" {
   description = "Database name"
   type        = string
-  default     = "lampdb"
+  sensitive   = true
 }
 
 variable "db_username" {
   description = "Database username"
   type        = string
-  default     = "admin"
+  sensitive   = true
 }
 
 variable "db_password" {
   description = "Database password"
   type        = string
   sensitive   = true
-}
-
-variable "tags" {
-  description = "Common tags for all resources"
-  type        = map(string)
-  default = {
-    Project     = "LAMP-Stack"
-    Environment = "dev"
-  }
 }
